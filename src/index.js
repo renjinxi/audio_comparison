83f2d4e13ce92e7014453b476858b3acc9222f77
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-06-30 17:25:22 +0800

    feature: add text upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import levenshtein from 'fast-levenshtein';
 import diff_match_patch from "diff-match-patch"
 import axios from "axios"
+import base64 from "base-64"
 
 window.axios = axios
 
@@ -53,6 +54,77 @@ document.getElementById('fileInput').addEventListener('change', function() {
     }
 });
 
+document.getElementById('uploadButton').addEventListener('click', function() {
+    var fileInput = document.getElementById('fileInput');
+    var selectedFile = fileInput.files[0];
+
+    if (!selectedFile) {
+        alert('Please select an audio file first.');
+        return;
+    }
+
+    var uploadButton = document.getElementById('uploadButton');
+    uploadButton.classList.add('disabled'); // 禁用上传标签
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var base64Audio = event.target.result.split(',')[1];
+
+        // 上传音频文件并获取任务ID
+        createAudioTask(base64Audio, selectedFile.name)
+            .then(taskId => {
+                console.log('Task ID:', taskId);
+                return getAudioResult(taskId);
+            })
+            .then(result => {
+                console.log('Transcription Result:', result);
+                window.uploadedText = result; // Store the content in a global variable
+                window.uploadedText = cleanString(window.uploadedText);
+                document.getElementById('accuracyDisplay').textContent = "Accuracy: --%";
+                uploadButton.classList.remove('disabled'); // 任务完成，启用上传标签
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById('transcriptionResult').textContent = 'Error: ' + error.message;
+                uploadButton.classList.remove('disabled'); // 任务完成，启用上传标签
+            });
+    };
+    reader.readAsDataURL(selectedFile);
+});
+
+document.getElementById('uploadTextButton').addEventListener('click', function() {
+    if (window.uploadedText == -1) {
+        alert('Please Upload Text File.');
+        return;
+    }
+
+    var uploadTextButton = document.getElementById('uploadTextButton');
+    uploadTextButton.classList.add('disabled'); // 禁用上传标签
+    createTextTask(window.uploadedText)
+        .then(taskId => {
+            console.log('Task ID:', taskId);
+            return getTextResult(taskId);
+        })
+        .then(result => {
+            const audioBase64String = result;
+            const audioSrc = `data:audio/mpeg;base64,${audioBase64String}`;
+
+            // Set the source of the audio element
+            const audioPlayer = document.getElementById('audioPlayer');
+            const source = document.getElementById('audioSource');
+            source.src = audioSrc;
+            source.type = 'audio/mpeg';
+            audioPlayer.appendChild(source);
+            uploadTextButton.classList.remove('disabled'); // 任务完成，启用上传标签
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert("Error:", error);
+            // document.getElementById('transcriptionResult').textContent = 'Error: ' + error.message;
+            uploadTextButton.classList.remove('disabled'); // 任务完成，启用上传标签
+        });
+});
+
+
 document.getElementById('readyGo').addEventListener('click', function() {
     var audioPlayer = document.getElementById('audioPlayer');
     if (audioPlayer.paused) {
@@ -103,6 +175,114 @@ document.getElementById('compareText').addEventListener('click', function() {
     //document.getElementById("originalTextDisplay").textContent = window.uploadedText
     displayDifferences(userInput, window.uploadedText);
 });
+
+function createAudioTask(base64Audio, fileName) {
+    return new Promise((resolve, reject) => {
+        // 准备请求数据
+        const audioData = {
+            data: base64Audio,
+            file_name: fileName,
+        };
+
+        // 发起POST请求上传音频
+        axios.post('http://localhost:8888/api/audio_upload/', audioData)
+            .then(response => {
+                if (response.status === 200 && response.data.msg && response.data.msg.task_id) {
+                    resolve(response.data.msg.task_id);
+                } else {
+                    reject(new Error('Failed to create audio task'));
+                }
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function getAudioResult(taskId) {
+    return new Promise((resolve, reject) => {
+        function checkResult() {
+            axios.post('http://localhost:8888/api/get_result/', null, {
+                params: { task_id: taskId },
+            })
+                .then(response => {
+                    if (response.status === 200 && response.data.msg) {
+                        const { state, result } = response.data.msg;
+
+                        if (state === 'completed') {
+                            resolve(result);
+                        } else if (state === 'failed') {
+                            reject(new Error('Audio transcription failed'));
+                        } else {
+                            // 等待一段时间后重试
+                            setTimeout(checkResult, 5000);
+                        }
+                    } else {
+                        reject(new Error('Unexpected response structure'));
+                    }
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        }
+
+        checkResult();
+    });
+}
+
+function createTextTask(text) {
+    return new Promise((resolve, reject) => {
+        // 准备请求数据
+        const audioData = {
+            data: text,
+        };
+
+        // 发起POST请求上传音频
+        axios.post('http://localhost:8888/api/text_upload/', audioData)
+            .then(response => {
+                if (response.status === 200 && response.data.msg && response.data.msg.task_id) {
+                    resolve(response.data.msg.task_id);
+                } else {
+                    reject(new Error('Failed to create audio task'));
+                }
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function getTextResult(taskId) {
+    return new Promise((resolve, reject) => {
+        function checkResult() {
+            axios.post('http://localhost:8888/api/get_result/', null, {
+                params: { task_id: taskId },
+            })
+                .then(response => {
+                    if (response.status === 200 && response.data.msg) {
+                        const { state, result } = response.data.msg;
+
+                        if (state === 'completed') {
+                            resolve(result);
+                        } else if (state === 'failed') {
+                            reject(new Error('Text transcription failed'));
+                        } else {
+                            // 等待一段时间后重试
+                            setTimeout(checkResult, 5000);
+                        }
+                    } else {
+                        reject(new Error('Unexpected response structure'));
+                    }
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        }
+
+        checkResult();
+    });
+}
+
 function calculateMatchPercentage(s1, s2) {
     var distance = window.levenshtein.get(s1, s2);
     var maxLength = Math.max(s1.length, s2.length);
